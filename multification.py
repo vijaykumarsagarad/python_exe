b3ab3e4065a0c2eaa1463b214a46e4c7513ea5d6
vijaykumarsagarad <52849853+vijaykumarsagarad@users.noreply.github.com>
2019-07-14 12:13:45 +0530

    multification.py

--- multification.py
+++ multification.py
@@ -1,16 +1,15 @@
 n=17
-	v="x"
-	s="="
-	for r in range(1,21):
-  p=n*r
-	    print(n,v,r,s,p)
-	"""
-	n=17
-	v='x'
-	s='='
-	r=1
-	while r<=10:
-	    p=n*r
-	    print(n,v,r,s,p)
-	    r=r+
-  
+v="x"
+s="="
+for r in range(1,11):
+    p=n*r
+    print(n,v,r,s,p)
+
+n=17
+v='x'
+s='='
+r=1
+while r<=10:
+    p=n*r
+    print(n,v,r,s,p)
+    r=r+1
